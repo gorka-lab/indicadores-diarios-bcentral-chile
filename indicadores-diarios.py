e8fe9948d3b8c3bd93313c3a9f2f44d5b4f17fe8
Jorge <jorge@algorithmlab.co>
2019-09-30 17:46:18 -0300

    Indicadores Diarios en una Coleccion

---
+++ indicadores-diarios.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import date
+
+result = requests.get("https://www.bcentral.cl/")
+
+if result.status_code != 200:
+	print("No se pudo establecer comunicación con la web")
+	quit()
+
+src = result.content
+
+soup = BeautifulSoup(src, 'lxml')
+
+div = soup.find_all("div", {"class":"tab-pane active", "id":"tab-1"})
+
+tds = []
+
+for table in div:
+	td_tag = table.find_all("td")
+	for td in td_tag:
+		tds.append(td.text)
+
+hoy = date.today()	
+indicadores = { 'Fecha' : str(hoy), "UF" : tds[2], "UTM" : tds[4], "Dolar" : tds[6], "Euro" : tds[8], "TCM" : tds[10]}
+
+print(indicadores)
